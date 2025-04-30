d66327bd5b46c5a64edf8246de1e9e0dabe7ba1c
SimoneMesiha <53223014+SimoneMesiha@users.noreply.github.com>
2025-04-29 22:56:03 -0400

    runners change

--- src/pages/Runners.jsx
+++ src/pages/Runners.jsx
@@ -3,13 +3,13 @@ import sponsorRulesImage from "../assets/HowToSponsor.jpg"; // Update path as ne
 import sponsorInstruction from "../assets/HowToGetSponsored.jpg";
 import qrCodeImage from "../assets/venmo.jpg"; // Update path as needed
 
-import mapImage1 from "../runners/5K Sponsors posts 2k24 - 10.jpg";
+import mapImage7 from "../runners/5K Sponsors posts 2k24 - 10.jpg";
 import mapImage2 from "../runners/5K Sponsors posts 2k24 - 11.jpg";
 import mapImage3 from "../runners/5K Sponsors posts 2k24 - 12.jpg"
 import mapImage4 from "../runners/5K Sponsors posts 2k24 - 13.jpg"
 import mapImage5 from "../runners/5K Sponsors posts 2k24 - 14.jpg"
 import mapImage6 from "../runners/5K Sponsors posts 2k24 - 15.jpg"
-import mapImage7 from "../runners/5K Sponsors posts 2k24 - 16.jpg"
+import mapImage1 from "../runners/Baggio.png"
 import mapImage8 from "../runners/5K Sponsors posts 2k24 - 17.jpg"
 import mapImage9 from "../runners/5K Sponsors posts 2k24 - 18.jpg"
 import mapImage10 from "../runners/IMG_7555.jpg"
@@ -26,88 +26,88 @@ const Runners = () => {
     {
       id: 1,
       name: "Runner 1",
-      imageUrl: mapImage8,
+      imageUrl: mapImage1,
     },
     {
       id: 2,
       name: "Runner 2",
-      imageUrl: mapImage1,
+      imageUrl: mapImage5,
     },
     {
       id: 3,
       name: "Runner 3",
-      imageUrl: mapImage7,
+      imageUrl: mapImage3,
     },
     {
       id: 4,
       name: "Runner 4",
       imageUrl: mapImage4,
-    },
-    {
-      id: 5,
-      name: "Runner 5",
-      imageUrl: mapImage5,
-    },
-    {
-      id: 6,
-      name: "Runner 6",
-      imageUrl: mapImage6,
-    },
-    {
-      id: 7,
-      name: "Runner 7",
-      imageUrl: mapImage2,
-    },
-    {
-      id: 8,
-      name: "Runner 8",
-      imageUrl: mapImage3,
-    },
-    {
-      id: 9,
-      name: "Runner 9",
-      imageUrl: mapImage9,
-    },
-    {
-      id: 10,
-      name: "Runner 10",
-      imageUrl: mapImage10,
-    },
-    {
-      id: 11,
-      name: "Runner 11",
-      imageUrl: mapImage11,
-    },
-    {
-      id: 12,
-      name: "Runner 12",
-      imageUrl: mapImage12,
-    },
-    {
-      id: 13,
-      name: "Runner 13",
-      imageUrl: mapImage13,
-    },
-    {
-      id: 14,
-      name: "Runner 14",
-      imageUrl: mapImage14,
-    },
-    {
-      id: 15,
-      name: "Runner 15",
-      imageUrl: mapImage15,
-    },
-    {
-      id: 16,
-      name: "Runner 16",
-      imageUrl: mapImage16,
-    },
-    {
-      id: 17,
-      name: "Runner 17",
-      imageUrl: mapImage17,
     }
+    // {
+    //   id: 5,
+    //   name: "Runner 5",
+    //   imageUrl: mapImage5,
+    // },
+    // {
+    //   id: 6,
+    //   name: "Runner 6",
+    //   imageUrl: mapImage6,
+    // },
+    // {
+    //   id: 7,
+    //   name: "Runner 7",
+    //   imageUrl: mapImage2,
+    // },
+    // {
+    //   id: 8,
+    //   name: "Runner 8",
+    //   imageUrl: mapImage3,
+    // },
+    // {
+    //   id: 9,
+    //   name: "Runner 9",
+    //   imageUrl: mapImage9,
+    // },
+    // {
+    //   id: 10,
+    //   name: "Runner 10",
+    //   imageUrl: mapImage10,
+    // },
+    // {
+    //   id: 11,
+    //   name: "Runner 11",
+    //   imageUrl: mapImage11,
+    // },
+    // {
+    //   id: 12,
+    //   name: "Runner 12",
+    //   imageUrl: mapImage12,
+    // },
+    // {
+    //   id: 13,
+    //   name: "Runner 13",
+    //   imageUrl: mapImage13,
+    // },
+    // {
+    //   id: 14,
+    //   name: "Runner 14",
+    //   imageUrl: mapImage14,
+    // },
+    // {
+    //   id: 15,
+    //   name: "Runner 15",
+    //   imageUrl: mapImage15,
+    // },
+    // {
+    //   id: 16,
+    //   name: "Runner 16",
+    //   imageUrl: mapImage16,
+    // },
+    // {
+    //   id: 17,
+    //   name: "Runner 17",
+    //   imageUrl: mapImage17,
+    // },
     // Add more runners as needed
   ];
 
@@ -149,13 +149,16 @@ const Runners = () => {
 
       {/* Runners Gallery */}
       <div className="flex flex-wrap -mx-2">
-        {runners.map((runner, index) => (
-          <div key={index} className="w-full sm:w-1/2 p-2">
-            <div style={{ height: "350px" }} className="w-full overflow-hidden">
+        {runners.map((runner) => (
+          <div
+            key={runner.id}
+            className="w-full sm:w-1/2 md:w-1/3 lg:w-1/4 p-2"
+          >
+            <div className="h-[350px] w-full overflow-hidden rounded-lg shadow-sm">
               <img
                 src={runner.imageUrl}
-                alt={`Runner ${index}`}
-                className="w-full h-full object-contain"
+                alt={`Runner ${runner.id}`}
+                className="w-full h-full object-cover"
               />
             </div>
           </div>
